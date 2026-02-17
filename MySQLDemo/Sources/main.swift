33bab86a2f9425bc3732be5869e6165aaf5a856f
syntaxxerrr <syreelmontajes@gmail.com>
2026-02-17 18:23:37 +0000

    Add shutdown handler to main.swift

--- MySQLDemo/Sources/main.swift
+++ MySQLDemo/Sources/main.swift
@@ -1,4 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Vapor
+import Foundation
 
-print("Hello, world!")
+var env = try Environment.detect()
+let app = Application(env)
+defer { app.shutdown() }
+
+try configure(app)
+
+// Catch Ctrl+C (SIGINT) and SIGTERM
+let signalQueue = DispatchQueue(label: "signal-handler")
+let sigintSource = DispatchSource.makeSignalSource(signal: SIGINT, queue: signalQueue)
+let sigtermSource = DispatchSource.makeSignalSource(signal: SIGTERM, queue: signalQueue)
+
+sigintSource.setEventHandler {
+    print("Received SIGINT, shutting down...")
+    app.shutdown()
+    exit(0)
+}
+sigtermSource.setEventHandler {
+    print("Received SIGTERM, shutting down...")
+    app.shutdown()
+    exit(0)
+}
+
+signal(SIGINT, SIG_IGN)   // ignore default handling
+signal(SIGTERM, SIG_IGN)  // ignore default handling
+sigintSource.resume()
+sigtermSource.resume()
+
+try app.run()
